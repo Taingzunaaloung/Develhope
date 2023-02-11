70e2463512a58f04869a71db0ffcdcd171a32296
Taingzunaaloung <taingzunaaloungfrancis@gmail.com>
2023-02-11 14:25:51 +0300

    Classes and Objects - Exercise 2

--- Python_Courses/Develhope Exercises/ex_01.py
+++ Python_Courses/Develhope Exercises/ex_01.py
@@ -327,3 +327,24 @@ class Animal:
 
 animal = Animal(4)
 animal.runs()
+
+print ('Classes and Objects - Exercise 2')
+class Animal:
+    def __init__ (self, legs_count):
+        print ('Animal object was created')
+        self.number_of_legs = legs_count
+   
+    def runs (self):
+        print ('Running started')
+    
+    def count_legs(self):
+        print(f"It has {self.number_of_legs} legs")
+   
+    def return_legs(self):
+        return self.number_of_legs
+
+animal = Animal(4)
+animal.runs()
+animal.count_legs()
+number_of_legs = animal.return_legs()
+print('Number of legs:',number_of_legs)
